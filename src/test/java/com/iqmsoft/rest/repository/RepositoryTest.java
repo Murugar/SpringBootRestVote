660aec1a69ee0ed89ac510886a89258b904abe43
Davanon <davanon2014@gmail.com>
2018-01-23 21:59:51 +0530

    First

--- src/test/java/com/iqmsoft/rest/repository/RepositoryTest.java
+++ src/test/java/com/iqmsoft/rest/repository/RepositoryTest.java
@@ -12,6 +12,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.iqmsoft.rest.MainTests;
 import com.iqmsoft.rest.entity.Restaurant;
+import com.iqmsoft.rest.entity.User;
+import com.iqmsoft.rest.entity.Vote;
+import com.iqmsoft.rest.entity.VoteResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +24,15 @@ public class RepositoryTest extends MainTests {
 
     @Autowired
     private RestaurantRepository rrepos;
+    
+    @Autowired
+    private UserRepository urepos;
+    
+    @Autowired
+    private VoteRepository vrepos;
+    
+    @Autowired
+    private VoteResultRepository vresrepos;
      
 
     @Before
@@ -38,7 +50,72 @@ public class RepositoryTest extends MainTests {
     }
     
     @Test
-    public void testSaveAndQuery(){
+    public void testUserSaveAndQuery(){
+    
+       User l = urepos.findOne(1l);
+       
+       Assert.assertNotNull(l);
+       
+       User uk = new User("test8", "test8") ;
+       
+       User uk2 = urepos.save(uk);
+       
+     
+       
+       Assert.assertNotNull(urepos.findById(uk2.getId()));
+       
+    
+    }
+    
+    @Test
+    public void testVotesSaveAndQuery(){
+    
+       User l = urepos.findOne(1l);
+       
+       Assert.assertNotNull(l);
+       
+       User uk = new User("test7", "test7") ;
+       
+       User uk2 = urepos.save(uk);
+       
+       Restaurant lk = new Restaurant("test40", "Chinese", "http://test.com/one.png", 12);
+       
+       Restaurant lk2 = rrepos.save(lk);
+       
+       Vote v = new Vote(uk, lk);
+       
+       
+       Vote v2 = vrepos.save(v);
+       
+       Assert.assertNotNull(uk2);
+       Assert.assertNotNull(lk2);
+       Assert.assertNotNull(v2);
+    
+    }
+
+    @Test
+    public void testVoteResultSaveAndQuery(){
+    
+       Restaurant l = rrepos.findOne(1l);
+       Assert.assertNotNull(l);
+       
+       Restaurant lk = new Restaurant("test40", "Chinese", "http://test.com/one.png", 12);
+       
+       Restaurant lk2 = rrepos.save(lk);
+       
+       VoteResult vr1 = new VoteResult(lk, 4l);
+       
+       VoteResult vr = vresrepos.save(vr1);
+       
+      
+       
+       Assert.assertNotNull(vr);
+       
+    
+    }
+    
+    @Test
+    public void testRestaurantSaveAndQuery(){
     
        Restaurant l = rrepos.findOne(1l);
        Assert.assertNotNull(l);
